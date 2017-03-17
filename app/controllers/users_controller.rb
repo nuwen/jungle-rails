51cc472cc7b8c1f25a1fcbe4c50d2f92a5c6eebb
Danton Nguyen <dantonnguyen88@gmail.com>
2017-03-17 03:42:24 +0000

    review posting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ private
 
   def user_params
 
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316234035) do
+ActiveRecord::Schema.define(version: 20170317031949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 20170316234035) do
     t.datetime "updated_at",      null: false
     t.string   "email"
     t.string   "password_digest"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_foreign_key "line_items", "orders"
