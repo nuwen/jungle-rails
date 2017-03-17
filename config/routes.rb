8576756885c3dfad583995629e1faec6faf23b36
Danton Nguyen <dantonnguyen88@gmail.com>
2017-03-17 02:02:30 +0000

    html review formatting and routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :reviews, only: [:create]
+  end
   resources :categories, only: [:show]
-  resources :reviews, only:[:show]
 
   resource :cart, only: [:show] do
     put    :add_item
